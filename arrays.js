6a7ddcb7b0c48ad100b766b6981c31da6b55bf1a
Christopher Beltran <christod.beltran@gmail.com>
2018-05-08 04:05:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,26 @@ var chocolateBars = [
   'skittles'
   ];
   
-function addElementToBeginningOfArray () {
+function addElementToBeginningOfArray (arrayName, newElement) {
+  [newElement, ...arrayName];
+  return arrayName;
+}
+
+function destructivelyAddElementToBeginningOfArray (theArray, newElement) {
+  theArray.unshift(newElement);
+  return theArray;
+}
+
+function addElementToEndOfArray (otherArray, endElement) {
+  [...otherArray, endElement];
+  return otherArray;
+}
+
+function destructivelyAddElementToEndOfArray (lastArray, lastElement) {
+  lastArray.push(lastElement);
+  return lastArray;
+}
+
+function accessElementInArray () {
   
 }
